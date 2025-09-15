94a43b74546f1c18748d3a764351612aaa62f346
Senén <senenrodero@gmail.com>
2025-09-15 16:32:14 +0200

    Add San Cugat repository URL

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.19.2"
 
 set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:Osoigo/decidim-sant_cugat.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
