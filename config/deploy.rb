e44a1b4421c7cb41da6ef8fffb530db54f87ae53
Senén <senenrodero@gmail.com>
2025-09-16 12:00:51 +0200

    Link the .env file in the shared folder
    We are using the .env file in the same way secrets is usually used.

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :format_options, command_output: true, log_file: "log/capistrano.log", color
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/secrets.yml'
+append :linked_files, ".env"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "storage", ".bundle"
