d5b8e261399f1c0b936e3544567ef84909343603
Senén <senenrodero@gmail.com>
2025-11-20 10:32:22 +0100

    Remove active storage override

--- config/initializers/aws_sdk_service.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require "aws-sdk-s3"
-require "active_storage/client_service_initialization_overrides"
-
-Aws::S3::Resource.class_eval do
-  prepend ActiveStorage::ClientServiceInitializationOverrides
-end
